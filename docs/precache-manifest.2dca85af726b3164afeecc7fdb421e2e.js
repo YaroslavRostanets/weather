88371e853df0689e510f15f1a3416d5d6ce0ca89
YaroslavRostanets <rostanets@gmail.com>
2019-01-30 02:23:12 +0200

    fix

---
+++ docs/precache-manifest.2dca85af726b3164afeecc7fdb421e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ce0fc016415513d8dce",
+    "url": "/static/js/main.2ce0fc01.chunk.js"
+  },
+  {
+    "revision": "b952d4d95997c60d27b2",
+    "url": "/static/js/1.b952d4d9.chunk.js"
+  },
+  {
+    "revision": "2ce0fc016415513d8dce",
+    "url": "/static/css/main.40860a40.chunk.css"
+  },
+  {
+    "revision": "8ce829813541df73818a7db57b730f47",
+    "url": "/index.html"
+  }
+];
